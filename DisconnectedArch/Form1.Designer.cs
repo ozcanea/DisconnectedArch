f8e62df9b91ce5a20293b75c06ee25dc2f639340
ozcanea <ekinanilozcan@gmail.com>
2022-02-13 19:21:42 +0300

    Add project files.

---
+++ DisconnectedArch/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace DisconnectedArch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCat = new System.Windows.Forms.Button();
+            this.btnEmp = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCat
+            // 
+            this.btnCat.Location = new System.Drawing.Point(146, 198);
+            this.btnCat.Name = "btnCat";
+            this.btnCat.Size = new System.Drawing.Size(241, 23);
+            this.btnCat.TabIndex = 0;
+            this.btnCat.Text = "Category List-DataTable";
+            this.btnCat.UseVisualStyleBackColor = true;
+            this.btnCat.Click += new System.EventHandler(this.btnCat_Click);
+            // 
+            // btnEmp
+            // 
+            this.btnEmp.Location = new System.Drawing.Point(146, 236);
+            this.btnEmp.Name = "btnEmp";
+            this.btnEmp.Size = new System.Drawing.Size(241, 23);
+            this.btnEmp.TabIndex = 1;
+            this.btnEmp.Text = "Employee List-DataSet";
+            this.btnEmp.UseVisualStyleBackColor = true;
+            this.btnEmp.Click += new System.EventHandler(this.btnEmp_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(498, 180);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 278);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnEmp);
+            this.Controls.Add(this.btnCat);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCat;
+        private System.Windows.Forms.Button btnEmp;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DisconnectedArch/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace DisconnectedArch
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //You have to change your win autentication's pwd and userid in app.config
+        DataSet ds = new DataSet();
+        DataTable dt = new DataTable();
+        private void btnEmp_Click(object sender, EventArgs e)
+        {
+            string command = "SELECT EmployeeID,FirstName,LastName,HomePhone,Photo FROM Employees";
+            string name = "Employees_table";
+            SqlBasicsDataSet(command,name);
+
+        }
+
+        private void btnCat_Click(object sender, EventArgs e)
+        {
+            string command = "Select * FROM Categories";
+            string name = "Categories_table";
+            SqlBasicDataTable(command, name);
+        }
+
+
+        #region DataSet
+        void SqlBasicsDataSet(string command,string tableName)
+        {
+
+            try
+            {
+                SqlDataAdapter da = new SqlDataAdapter(command, ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+                
+                if (ds.Tables[tableName] == null)
+                {   
+                    da.Fill(ds, tableName);
+                    // dataGridView1.DataSource = ds.Tables[0];
+                    dataGridView1.DataSource = ds;
+                    dataGridView1.DataMember = tableName;
+                }
+                else
+                {
+                    
+                    dataGridView1.DataSource = ds.Tables[tableName];
+                   // dataGridView1.DataMember = dt.TableName;
+                }
+                da.Dispose();
+
+            }
+            
+            catch(SystemException sye)
+            {
+                MessageBox.Show(sye.Message);
+            }
+            
+            
+        }
+        #endregion
+
+        #region DataTable
+
+        
+        void SqlBasicDataTable(string command, string tableName)
+        {
+
+            SqlDataAdapter da = new SqlDataAdapter(command, ConfigurationManager.ConnectionStrings["con"].ConnectionString);
+            try
+            {
+                if (dt.TableName!=tableName)
+                {
+                    da.Fill(dt);
+                    dataGridView1.DataSource = dt;
+                    dt.TableName= tableName;
+                }
+                else
+                {
+                    dataGridView1.DataSource = dt;
+                    
+                }
+                da.Dispose();
+                
+            }
+            catch (InvalidOperationException ioe)
+            {
+                MessageBox.Show(ioe.Message);
+            }
+        }
+        #endregion
+    }
+}
